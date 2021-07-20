9b55529f101020399f9ab41583666feab13e3055
Ramsey Njire <ramseynjire@gmail.com>
2021-07-20 21:18:40 +0300

    run migration to create goals table

--- db/migrate/20210720181533_create_goals.rb
+++ db/migrate/20210720181533_create_goals.rb
@@ -1,7 +1,11 @@
 class CreateGoals < ActiveRecord::Migration[5.2]
   def change
     create_table :goals do |t|
-
+      t.string :title, null: false
+      t.text :description, null: false
+      t.boolean :completed, null: false, default: false
+      t.boolean :private, null: false, default: false
+      t.integer :creator_id, null: false, user: :references
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_18_144419) do
+ActiveRecord::Schema.define(version: 2021_07_20_181533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "goals", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description", null: false
+    t.boolean "completed", default: false, null: false
+    t.boolean "private", default: false, null: false
+    t.integer "creator_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
