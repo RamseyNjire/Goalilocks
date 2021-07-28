81574545a44758c71cda1ddf9d802feb6f1d3ad1
Ramsey Njire <ramseynjire@gmail.com>
2021-07-28 18:31:15 +0300

    write migration to create comments table

--- db/migrate/20210727205010_create_comments.rb
+++ db/migrate/20210727205010_create_comments.rb
@@ -1,6 +1,8 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
+      t.text :body, null: false
+      t.references :commentable, polymorphic: true
 
       t.timestamps
     end
