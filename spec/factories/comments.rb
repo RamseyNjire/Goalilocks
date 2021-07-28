3ad268a56e71e704a344c253f149fac398fbabe7
Ramsey Njire <ramseynjire@gmail.com>
2021-07-28 19:07:00 +0300

    write migration that allows comments to have an association with the users that write them

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :comment do
-    
+    trait :for_goal do
+      association :commentable, factory: :goal
+    end
+
+    trait :for_user do
+      association :commentable, factory: :user
+    end
   end
 end
