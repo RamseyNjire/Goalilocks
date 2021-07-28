141aa5e2eecdaa34e7ab4af70ecaa0b8e4a0593c
Ramsey Njire <ramseynjire@gmail.com>
2021-07-28 19:08:37 +0300

    write factory for comments, with separate traits for goals and users (which are both commentable

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :comment do
+    writer { create(user) }
+    body { "This is a test comment" }
     trait :for_goal do
       association :commentable, factory: :goal
     end
