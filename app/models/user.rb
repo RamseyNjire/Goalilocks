08edf04404c865dfeb3826d294e914ab82bd2392
Ramsey Njire <ramseynjire@gmail.com>
2021-08-06 11:55:37 +0300

    create commentable module as a concern

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,7 +29,7 @@ class User < ApplicationRecord
         dependent: :destroy
     )
 
-    has_many :comments, as: :commentable
+    has_many :comments, as: :commentable, dependent: :destroy
 
     has_many(
         :written_comments,
